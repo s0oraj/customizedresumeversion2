968188e3527d9fc3c5a5f3602f7c9c4e38a185b3
Suraj Kumar Singh <42529024+s0oraj@users.noreply.github.com>
2025-08-25 05:15:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Resume } from './components/Resume';
+import { PDFDownloadButton } from './components/PDFDownloadButton';
+
 export function App() {
-  return <div className="flex w-full min-h-screen bg-black">
+  return (
+    <div className="flex w-full min-h-screen bg-black">
       <Resume />
-    </div>;
+      <PDFDownloadButton />
+    </div>
+  );
 }
