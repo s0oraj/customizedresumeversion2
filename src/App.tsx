a802e114024bedf3b51c719b698266f9f0ef5805
suraj <ridesuraj@gmail.com>
2025-08-25 09:45:33 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Resume } from './components/Resume';
-import { PuppeteerPDFButton } from './components/PuppeteerPDFButton';
+
 
 export function App() {
   return (
     <div className="flex w-full min-h-screen bg-black">
       <Resume />
-      <PuppeteerPDFButton />
     </div>
   );
 }
